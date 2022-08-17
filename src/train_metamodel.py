0ba5bb24eba286e7b677918877836f087ff835b9
azagsam-cronos <ales.zagar@hotmail.com>
2022-08-17 14:34:14 +0000

    GPU support

--- src/train_metamodel.py
+++ src/train_metamodel.py
@@ -5,8 +5,8 @@ import numpy as np
 import pandas as pd
 import tensorflow as tf
 import yaml
-from keras.layers import Dense
-from keras.models import Sequential
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.models import Sequential
 from sklearn.model_selection import train_test_split
 
 
