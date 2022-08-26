1a4266da3cc99f685252e1238a30cb5d71333b95
azagsam-cronos <ales.zagar@hotmail.com>
2022-08-26 14:21:51 +0000

    Balanced model

--- src/balance_dataset.py
+++ src/balance_dataset.py
@@ -1,5 +1,7 @@
 import pandas as pd
 
 df = pd.read_json('data/metamodel-training-with-scores-large.jsonl', lines=True)
-
-print(df.head())
+df = df[df['source'] != 'surs']
+g = df.groupby('source')
+balanced = g.apply(lambda x: x.sample(g.size().min()).reset_index(drop=True))
+balanced.to_json('data/metamodel-training-with-scores-balanced.jsonl', lines=True, orient='records', force_ascii=False)
